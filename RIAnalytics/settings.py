aa4155678ff1333174cbb9f2c1de788cae45d9b3
Dhairya Patel <dhairya00798@gmail.com>
2022-06-11 13:25:07 +0530

    csrf trusted origin

--- RIAnalytics/settings.py
+++ RIAnalytics/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ["https://rianalytics.wonderfulsea-39291c4b.eastasia.azurecontainerapps.io/text/textform"]
+CSRF_TRUSTED_ORIGINS = ["https://rianalytics.wonderfulsea-39291c4b.eastasia.azurecontainerapps.io"]
 
 # Application definition
 
